2f64d55bf5f82630d716a054636ef83653049295
Daniel Perez <daniel@perez.sh>
2019-03-07 01:47:16 +0000

    Add script to extract information from results

--- scripts/get_miner_info.py
+++ scripts/get_miner_info.py
@@ -26,16 +26,19 @@ def aggregate_results(data, address):
     if not miner:
         raise ValueError("miner {0} does not exist".format(address))
     miner = miner[0]
-    pool_data = [m for v in data["pools"] for m in v["miners"] if m["address"] == address]
+    pool_data = [(m, p) for p in data["pools"] for m in p["miners"] if m["address"] == address]
     if not pool_data:
         raise ValueError("miner not in any pool")
-    blocks_received = sum(v["metadata"]["blocks_received"] for v in pool_data)
-    blocks_mined = sum(v["metadata"]["blocks_mined"] for v in pool_data)
+    blocks_received = sum(m["metadata"]["blocks_received"] for m, _ in pool_data)
+    blocks_mined = sum(m["metadata"]["blocks_mined"] for m, _ in pool_data)
+    pool_work = {p["name"]: m["metadata"]["share_count"] * p["difficulty"] / m["metadata"]["blocks_received"] for m, p in pool_data}
     return dict(
         blocks_mined=blocks_mined,
         blocks_received=blocks_received,
         blocks_ratio=blocks_received / blocks_mined,
-        work_per_block=miner["total_work"] / blocks_received
+        work_per_block=miner["total_work"] / blocks_received,
+        work_in_pool_per_block=pool_work,
+        work_per_block_solo=miner["total_work"] / blocks_mined
     )
 
 
